cb1033f7f1fe8d302d54a123bb48621c7b35d0ee
Gabriel <gabrielestevo92@gmail.com>
2022-02-02 14:47:50 -0300

    Final

--- script.js
+++ script.js
@@ -5,9 +5,18 @@ sinal = '';
 
 function tela(valor){    
     if (valor == "+" || valor == '-' || valor == 'X' || valor == '/'){
-        sinal = valor;
-        num_1 = numero;
-        numero = '';
+        if (num_1 == ''){
+            sinal = valor;
+            num_1 = numero;
+            numero = '';
+            console.log("Olá");
+        }
+        else{
+            sinal = valor;
+            numero = '';
+
+        }
+        
     }
     // 100/50 * 10
     else if (valor == "=" || valor == "%"){
@@ -23,32 +32,28 @@ function tela(valor){
             numero = '';
             switch (sinal) {
                 case '+':
-                    somar(num_1,num_2);                
-                    num_1 = '';
-                    num_2 = '';
+                    num_1 = somar(num_1,num_2);                
+                    num_2 = '';                    
                     sinal = '';
                     break;
                 case '-':
-                    subtrair(num_1,num_2);                
-                    num_1 = '';
+                    
+                    num_1 = subtrair(num_1,num_2);                
                     num_2 = '';
                     sinal = '';
                     break;
-                case 'X':
-                    multiplicar(num_1,num_2);                
-                    num_1 = '';
+                case 'X':                                    
+                    num_1 = multiplicar(num_1,num_2);
                     num_2 = '';
                     sinal = '';
                     break;
-                case '/':
-                    dividir(num_1,num_2);                
-                    num_1 = '';
+                case '/':                    
+                    num_1 = dividir(num_1,num_2);
                     num_2 = '';
                     sinal = '';
                     break;
-                case '%':
-                    por_cento(num_1,num_2);                
-                    num_1 = '';
+                case '%':                    
+                    num_1 = por_cento(num_1,num_2);                
                     num_2 = '';
                     sinal = '';
                     break;
@@ -62,7 +67,18 @@ function tela(valor){
             numero = (numero) * -1;
             document.getElementById("na-tela").innerHTML = numero;
         }
+        else if(valor == '.' && numero == ''){
+            numero = '0.';
+            document.getElementById("na-tela").innerHTML = numero;
+        }
+        else if(valor == '.' && numero == ''){
+            numero = '0.';
+            document.getElementById("na-tela").innerHTML = numero;
+        }
         else{
+            if (sinal == '' && num_1 != ''){
+                num_1 = '';
+            }
             numero = numero + valor;
             document.getElementById("na-tela").innerHTML = numero;
         }
@@ -71,19 +87,21 @@ function tela(valor){
 
 function somar(num_1,num_2){
     document.getElementById("na-tela").innerHTML = parseFloat(num_1) + parseFloat(num_2);
+    return parseFloat(num_1) + parseFloat(num_2);
 }
 function subtrair(num_1,num_2){
     document.getElementById("na-tela").innerHTML = parseFloat(num_1) - parseFloat(num_2);
+    return parseFloat(num_1) - parseFloat(num_2);
 }
 function multiplicar(num_1,num_2){
     document.getElementById("na-tela").innerHTML = parseFloat(num_1) * parseFloat(num_2);
+    return parseFloat(num_1) * parseFloat(num_2);
 }
 function dividir(num_1,num_2){
     document.getElementById("na-tela").innerHTML = parseFloat(num_1) / parseFloat(num_2);
+    return parseFloat(num_1) / parseFloat(num_2);
 }
-function por_cento(num_1,num_2){
-    document.getElementById("na-tela").innerHTML = parseFloat(num_1) / parseFloat(num_2);
-}
+
 
 function limpar_tela() {
     document.getElementById("na-tela").innerHTML = 0;
